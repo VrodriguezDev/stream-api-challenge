9c9478213dcd53386480b5878408792af82e12de
dev.VR <dev.vrodriguez@gmail.com>
2023-12-06 17:29:27 -0400

    imports

--- src/unosquare/coe/challenge/Main.java
+++ src/unosquare/coe/challenge/Main.java
@@ -5,7 +5,9 @@ import unosquare.coe.challenge.model.Order;
 import unosquare.coe.challenge.model.Product;
 import unosquare.coe.challenge.util.PopulateData;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
